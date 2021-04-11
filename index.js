fd4ac14849f8d3347ef3dbdbbeae8f5ee8a18d0e
Santosh Divate <dsantoshkumarit@gmail.com>
2021-04-11 11:05:59 +0530

    masked dburl

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ import theaterRoutes from "./routes/admin/theater.js";
 import movieRoutes from "./routes/admin/movie.js";
 
 const app = Express();
-const port = 3002;
-const dburl =
-  "mongodb+srv://Santosh_Dev:9lhl74dXO5zqkxJi@cluster0.xyoew.mongodb.net/BookMyShow?retryWrites=true&w=majority";
 
-mongoose.connect(dburl, { useNewUrlParser: true });
+const url = process.env.MONGODB_URI || "mongodb://localhost/RecipeData";
+const port = process.env.PORT || 3200;
+
+mongoose.connect(url, { useNewUrlParser: true });
 const con = mongoose.connection;
 con.on("open", () => {
   console.log("Mongo DB connected");
